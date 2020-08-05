185ba16f64186a0a400326f4d6f3d56fdda2a086
Yaki Koren <yaki.koren@gmail.com>
2020-08-05 15:54:54 +0300

    complete cycle

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,62 @@
 import * as sql from 'mssql'
+import * as ExcelJS from 'exceljs'
+import async from 'async';
+
+
 
 const config: sql.config = {
   user: 'yakik',
   password: 'yakik',
-  server: 'DESKTOP-HHU9APQ', // You can use 'localhost\\instance' to connect to named instance
+  server: 'DESKTOP-HHU9APQ',
   database: 'RevitP1',
   options: {
     enableArithAbort: true
   },
   beforeConnect: conn => {
-    conn.on('debug', message => console.info(message));
-    conn.on('error', err => console.error(err));
+    //conn.on('debug', message => console.info(message));
+    conn.on('error', err => console.error('CCDCCD' + err));
   }
 }
 
+interface query {
+  sheetName: string,
+  query: string
+}
 
-let connection: sql.ConnectionPool = new sql.ConnectionPool(config, function (err: any) {
-  if (err != null) {
-    console.warn("Issue with connecting to SQL Server!");
-  }
-  else {
-    let requestQuery = new sql.Request(connection);
-    let getDataQuery = "select * from RevitP1.dbo.Floors";
 
-    requestQuery.query(getDataQuery, function (err, result) {
-      if (err) {
-        console.error(`Error happened calling Query: ${err.name} ${err.message}`);
-      }
-      // checking to see if the articles returned as at least one.
-      else if (result.recordset.length > 0) {
-        console.dir(result)
-      }
-    })
-  }
-})
-
-const ExcelJS = require('exceljs');
+let queries: Array<query> = [{ sheetName: 'mySum', query: 'select sum(Area) as sum_area,TypeId from RevitP1.dbo.Floors group by TypeId' },
+{ sheetName: 'myCount', query: 'select count(Area) as sum_area,TypeId from RevitP1.dbo.Floors group by TypeId' }]
 
 const workbook = new ExcelJS.Workbook();
 
-const sheet = workbook.addWorksheet('My Sheet');
+let connection: sql.ConnectionPool = new sql.ConnectionPool(config);
+connection.connect().then(() => {
+  async.eachSeries(queries, (query: query,callBack) => {
+    console.log(query)
+    const sheet = workbook.addWorksheet(query.sheetName);
+    connection.query(query.query).then((data) => {
+      let columns = Object.keys(data.recordset[0])
+      let worksheetColumns: Array<Object> = []
+      columns.forEach(column => {
+        worksheetColumns.push({ header: column, key: column })
+      })
+      sheet.columns = worksheetColumns
+      data.recordset.forEach(record => {
+        console.log(record)
+        sheet.addRow(record)
+      })
+      callBack()
+    })
+  }, (error) => {
+    connection.close()
+    console.log('HHHHH')
+    if (error)
+      console.error(error)
+    else
+      workbook.xlsx.writeFile('../myExcel.xlsx');
+      console.log('HHHHHEEEEE')
+  })
+})
 
-const cell = sheet.getCell('C3');
 
-cell.value = 42
-
-workbook.xlsx.writeFile('../myExcel.xlsx');
 
